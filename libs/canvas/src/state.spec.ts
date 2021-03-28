f2ff9a84a97d04f9ee7c9cbe3413b753a8f71f14
Shuma Yoshioka <shuma_yoshioka@kiganix.co.jp>
2021-03-28 12:49:57 +0900

    Create initial performance time test

---
+++ libs/canvas/src/state.spec.ts
@@ -0,0 +1,24 @@
+import { StateResolver } from './consts'
+import { createInternalState, InternalState } from './state'
+
+describe('createInternalState', () => {
+
+  describe('初回描画につきstateがない場合', () => {
+    const state: InternalState | undefined = undefined
+    const stateResolver: StateResolver = () => { return { released: false } }
+
+    test('initialPerformanceTimeは常に現在のperformanceTimeになること', () => {
+      expect(
+        createInternalState(state, 0, stateResolver).initialPerformanceTime
+      ).toEqual(0)
+      expect(
+        createInternalState(state, 114514, stateResolver).initialPerformanceTime
+      ).toEqual(114514)
+      expect(
+        createInternalState(state, -114514, stateResolver).initialPerformanceTime
+      ).toEqual(-114514)
+    })
+
+  })
+
+})
